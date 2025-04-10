5a3c3af9d2345370f68e698792d473ef7a8c4d43
Keitoto <keitotooffice@gmail.com>
2025-04-10 19:22:14 +0200

    ✨ add Section Heading

---
+++ components/SectionHeading.tsx
@@ -0,0 +1,23 @@
+import type { ComponentPropsWithoutRef, FC } from 'react'
+
+type Props = {
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p'
+  subText?: string
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'h1'> // Extends HTML attributes
+
+export const SectionHeading: FC<Props> = ({
+  as: Tag = 'h1',
+  subText,
+  children,
+  ...props
+}) => {
+  return (
+    <div className="flex flex-col-reverse items-center gap-4 mb-8">
+      <Tag className="text-5xl leading-none" {...props}>
+        {children}
+      </Tag>
+      {subText && <p className="text-lg leading-none">{subText}</p>}
+    </div>
+  )
+}
