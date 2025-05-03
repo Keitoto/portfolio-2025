0495369accaaa2867e4c6ecd7fefc6a013d41ecf
Keitoto <keitotooffice@gmail.com>
2025-05-03 17:23:40 +0200

    🏷️ IWorkCard

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface IWorkCard {
+  title: string
+  description: string
+  tags: string[]
+  demoLink: string
+  githubLink: string
+  articleLink: string
+  thumbNail: string
+  id: string
+}
