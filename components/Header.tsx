491a846cbc2ce86a9e76ce90e777a427a2471ff6
Keitoto <keitotooffice@gmail.com>
2025-05-03 23:40:13 +0200

    ✨ mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react'
 import { ThemeToggle } from '@/components/ThemeToggle'
-import { buttonVariants } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { Sheet, SheetClose, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { Menu } from 'lucide-react'
 import Link from 'next/link'
 
 interface Props {}
@@ -44,6 +46,36 @@ export const Header: FC<Props> = () => {
       <div className="hidden md:flex ml-auto items-center gap-2">
         <ThemeToggle />
         <Link href="/#contact" className={`${buttonVariants({ variant: 'outline' })} !rounded-full h-12 px-8 !text-lg`}>Let's talk</Link>
+      </div>
+
+      {/* mobile */}
+      <div className="md:hidden flex items-center ml-auto gap-4">
+        <ThemeToggle />
+        <Sheet>
+          <SheetTitle className="sr-only">Mobile Menu</SheetTitle>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Menu className="size-8" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px] sm:w-[400px] pt-20 px-12">
+            <ul className="flex flex-col items-center gap-8 lg:gap-8 leading-none mb-8">
+              {navMenu.map((item) => {
+                return (
+                  <li key={item.name} className="hover:text-muted-foreground">
+                    <SheetClose asChild>
+                      <Link href={item.href}>{item.name}</Link>
+                    </SheetClose>
+                  </li>
+                )
+              })}
+            </ul>
+            <SheetClose>
+              <Link href="/#contact" className={`${buttonVariants({ variant: 'outline' })} !rounded-full h-12 px-8 !text-lg`}>Let's talk</Link>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   )
