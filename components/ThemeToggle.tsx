fca878be2295a744deec197581aac82c89264610
Keitoto <keitotooffice@gmail.com>
2025-05-07 16:06:19 +0200

    💄 changed style

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
 
-import { Moon, Sun } from 'lucide-react'
+import { MoonStar, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
@@ -20,24 +19,26 @@ export function ThemeToggle() {
 
   return (
     <Button
-      variant="outline"
+      variant="ghost"
       size="icon"
-      className="relative h-8 w-15 rounded-full p-0"
+      className="size-8 p-0"
       onClick={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}
     >
-      {/* Track */}
+      {resolvedTheme === 'dark' ? <Sun className="size-6" strokeWidth="1.5" /> : <MoonStar className="size-6" strokeWidth="1" />}
+      {/* toggle version */}
+      {/* Track
       <div className="absolute inset-0 flex items-center justify-between px-2">
         <Moon className="size-4" />
         <Sun className="size-4" />
       </div>
 
-      {/* Thumb (moving circle) */}
+      Thumb (moving circle)
       <div
         className={cn(
           'absolute left-1 size-5 rounded-full bg-primary transition-all',
           resolvedTheme === 'dark' ? 'translate-x-7' : 'translate-x-0',
         )}
-      />
+      /> */}
     </Button>
   )
 }
